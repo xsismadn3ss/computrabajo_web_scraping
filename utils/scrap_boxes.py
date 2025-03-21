5e3dc06e3f9450f020d9b2124599340af3bce741
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-03-21 16:33:16 -0600

    feat: add scrap_boxes function to extract offer details from HTML
    This commit introduces a new utility function `scrap_boxes` that parses HTML content to extract offer details such as title, company, location, and salary. The extracted data is then used to populate `Offer` objects and update the `research` object with relevant company and location information.

---
+++ utils/scrap_boxes.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+from bs4.element import ResultSet
+from data_types.offer import Offer
+import re
+from data_types.research import research
+
+
+def scrap_boxes(soup: BeautifulSoup, boxes) -> list[Offer]:
+    for box in boxes:
+        title: str = box.find("h2")
+        company = box.find("p", class_="dFlex vm_fx fs16 fc_base mt5").find("a")
+        location = box.find("p", class_="fs16 fc_base mt5").find("span")
+        published = box.find("p", class_="fs13")
+        miscs = box.find("div", class_="fs13 mt15")
+        salary: str = ""
+        mode: str = ""
+
+        if miscs is not None:
+            miscs = miscs.find_all("span", class_="dIB mr10")
+            for misc in miscs:
+                if re.search(r"\d+", misc.text.strip()):
+                    salary = misc.text.strip()
+                else:
+                    mode = misc.text.strip()
+
+        offer = Offer(title, company, location, mode, salary, published)
+        if offer.company:
+            research.companies.add(offer.company)
+        research.locations.add(offer.location)
+        research.offers.append(offer)
