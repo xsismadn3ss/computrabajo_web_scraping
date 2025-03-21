1bbaa6f78a1de1a8a1e7a10910720bb9797b6809
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-03-21 16:34:01 -0600

    chore: remove unused files scrap_boxes.py, offer.py, and test.py
    The files were no longer needed in the project as they were either unused or replaced by other implementations. This cleanup helps maintain a cleaner codebase and reduces unnecessary clutter.

--- offer.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-@dataclass
-class Offer:
-    company: str
-    location:str
-    mode: str
-    salary:str|None
-    published:str|None
--- scrap_boxes.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-from offer import Offer
-
-def scrap_boxes(soup:BeautifulSoup)-> list[Offer]:
-    pass
