984fe8c2c388cfd9e9713efa97e05c9652b5875f
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-03-22 16:18:35 -0600

    feat(cli): add CLI interface for job scraping with click library
    Introduce a CLI interface using the click library to allow users to specify job and location parameters dynamically. This enhances usability by enabling interactive input and modular command execution. The CLI supports commands for checking total jobs, scraping job data, and future implementations for company-specific searches.

--- main.py
+++ main.py
@@ -1,46 +1,79 @@
 import requests
 from bs4 import BeautifulSoup
 from data_types.offer import Offer
-from utils.scrap_page import scrap_page
+from utils.scrap_page import scrap_page, look_total_jobs
 from data_types.research import research
 from data_types.scrap_params import ScrapParams
+from icecream import ic
+import click
 
 scrap_params = ScrapParams(
-    url="https://sv.computrabajo.com/trabajo-de-programador-en-san-salvador?p={}",
+    url="https://sv.computrabajo.com/trabajo-de-{}-en-{}?p={}",
     headers={
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0"
     },
 )
-scrap_page(scrap_params)
-while True:
-    if scrap_params.actual_page > scrap_params.total_pages:
-        break
+
+
+@click.group()
+def cli():
+    """
+    --------------BIENVENIDO A COMPUTRABAJO SCRAPER CLI --------------
+    description - Aplicación de consola para obtener información de los trabajos de computrabajo
+    """
+    pass
+
+
+@cli.command()
+@click.option(
+    "--job", prompt="indica el trabajo que deseas buscar: ", help="Trabajo a buscar"
+)
+@click.option(
+    "--location",
+    prompt="indica la ubicación que deseas buscar: ",
+    help="Ubicación a buscar",
+    default="san salvador",
+)
+def total_jobs(job: str, location: str):
+    location = location.replace(" ", "-")
+    job = job.replace(" ", "-")
+    total = look_total_jobs(scrap_params, job, location)
+    print(f"{total} trabajos de {scrap_params.job.replace('-', ' ')}")
+
+
+@cli.command()
+@click.option(
+    "--job", prompt="indica el trabajo que deseas buscar ", help="Trabajo a buscar"
+)
+@click.option(
+    "--location",
+    prompt="indica la ubicación que deseas buscar ",
+    help="Ubicación a buscar",
+    default="san salvador",
+)
+def companies(job: str, location: str):
+    location = location.replace(" ", "-")
+    job = job.replace(" ", "-")
+    raise NotImplementedError(
+        "Implementar función para buscar empresas que ofrecen el trabajo buscado"
+    )
+
+
+@cli.command()
+@click.option(
+    "--job", prompt="indica el trabajo que deseas buscar: ", help="Trabajo a buscar"
+)
+@click.option(
+    "--location",
+    prompt="indica la ubicación que deseas buscar: ",
+    help="Ubicación a buscar",
+    default="san salvador",
+)
+def scrap(param: str, location: str):
+    location = location.replace(" ", "-")
+    param = job.replace(" ", "-")
     scrap_page(scrap_params)
 
-# Save scraped data to a text file
-print("Saving recolected data....")
-with open("scraped_jobs.txt", "w", encoding="utf-8") as f:
-    f.write(f"Total Job: {len(research.offers)}\n\n")
 
-    f.write(f"Locations ({len(research.locations)})\n")
-    for location in research.locations:
-        f.write(f"- {location}\n")
-    f.write("\n")
-
-    f.write(f"Companies ({len(research.companies)})\n")
-    for company in research.companies:
-        f.write(f"- {company}\n")
-    f.write("\n")
-
-    for offer in research.offers:
-        # Write each offer's details on a new line
-        f.write("-" * 50 + "\n")  # Separator between offers
-        f.write(f"{offer.title.upper()}\n")
-        f.write(f"Company: {offer.company}\n")
-        f.write(f"Location: {offer.location}\n")
-        f.write(f"Mode: {offer.mode}\n")
-        f.write(f"Salry: {offer.salary}\n")
-        f.write(f"Publisehd: {offer.published}\n")
-        f.write("-" * 50 + "\n")  # Separator between offers
-
-print("Data saved")
+if __name__ == "__main__":
+    cli()
