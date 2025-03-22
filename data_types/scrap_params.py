dc0807153749c17f17f57043dec529a23353a366
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-03-22 16:15:28 -0600

    feat(scrap_params): add job and location fields to ScrapParams
    This commit introduces new fields `job` and `location` to the `ScrapParams` class to support additional filtering criteria in scraping operations.

--- data_types/scrap_params.py
+++ data_types/scrap_params.py
@@ -2,9 +2,11 @@ from typing import Optional
 
 
 class ScrapParams:
-    def __init__(self, url: Optional[str] = None, headers: Optional[dict] = None):
+    def __init__(self, url: Optional[str] = None, headers: Optional[dict] = None, job: Optional[str] = None, location: Optional[str] = None):
         self.total_items = None
         self.total_pages = None
         self.actual_page = 1
         self.url = url
         self.headers = headers
+        self.job = job
+        self.location = location
