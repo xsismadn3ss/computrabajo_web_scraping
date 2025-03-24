c503f2df010e39f5e4d3b361ac43011a4d533125
Abraham <2477carlos.artiga@gmail.com>
2025-03-24 09:44:09 -0600

    feat(cli): add download functionality for job scraping and improve total jobs display

--- utils/scrap_page.py
+++ utils/scrap_page.py
@@ -4,27 +4,59 @@ from typing import List
 from .scrap_boxes import scrap_boxes
 import requests
 from data_types.scrap_params import ScrapParams
+import colorama
 
 
-def look_total_jobs(scrap_params: ScrapParams, job: str, loaction: str) -> str:
+def get_file(scrap_params: ScrapParams, job: str, location: str):
+    """Descargar archivo HTML y guardarlos en la carpeta ``downloads``
+
+    Args:
+        scrap_params (ScrapParams): instancia de clase con parametros de busqueda
+        job (str): trabajo buscado
+        location (str): ubicación para buscar trabajos por zona
+    """
     print(
-        f"Scraping: {scrap_params.url.format(job, loaction, scrap_params.actual_page)}...."
+        f"downloading {colorama.Fore.GREEN}{scrap_params.url.format(job, location, scrap_params.actual_page)}{colorama.Fore.RESET} ..."
     )
     res = requests.get(
-        url=scrap_params.url.format(job, loaction, scrap_params.actual_page),
+        url=scrap_params.url.format(job, location, scrap_params.actual_page),
         headers=scrap_params.headers,
     )
-    scrap_params.job = job
-    scrap_params.location = loaction
-
     if not res.ok:
-        return []
-    # guardar todo el html
+        return
+
     soup = BeautifulSoup(res.text, "html.parser")
+    total = soup.find("h1", class_="title_page").find("span")
+    scrap_params.total_items = int(total.text.strip().replace(" ", ""))
+    scrap_params.total_pages = scrap_params.total_items // 20 + 1
+
+    with open(
+        f"downloads/page_{scrap_params.actual_page}.html", "w", encoding="utf-8"
+    ) as file:
+        file.write(res.text)
+    scrap_params.actual_page += 1
+
+
+def look_total_jobs() -> str:
+    with open("downloads/page_1.html", "r", encoding="utf-8") as file:
+        doc = file.read()
+        file.close()
+
+    soup = BeautifulSoup(doc, "html.parser")
     total_div = soup.find("div", class_="box_title")
-    total_h1 = total_div.find("h1", class_="title_page").find("span")
-    total = total_h1.text.strip().replace(" ", "")
-    return total
+    total_h1 = total_div.find("h1", class_="title_page")
+    total_span = total_h1.find("span")
+    n: str = total_h1.text.strip()
+    words = n.split(" ")
+    text = ""
+    for word in words:
+        if word == "":
+            continue
+        if word.strip().isdigit():
+            text += f"{colorama.Fore.GREEN}{word.strip()}{colorama.Fore.RESET}"
+            continue
+        text += f"{word.strip()} "
+    return text
 
 
 def scrap_page(scrap_params: ScrapParams, job: str, location: str) -> None:
